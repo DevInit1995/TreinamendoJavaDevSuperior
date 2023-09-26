22d1780f5353e605ce16161823f336b12ca17761
Jhon Santos <kennedyalves871@gmail.com>
2023-09-26 18:34:07 -0300

    Preparação de conexão com o banco de dados

---
+++ src/DB/Program.java
@@ -0,0 +1,14 @@
+package DB;
+
+import java.sql.Connection;
+
+public class Program {
+
+	public static void main(String[] args) {
+	
+		Connection conn = DB.getConnection();
+		DB.closeConnection();
+
+	}
+
+}
